862d6c84fba6febb3f50b1dd9905495661227b00
mershter <100219272+mershter@users.noreply.github.com>
2022-04-30 23:09:04 +0200

    adding jquery and slide ins

--- jquery.js
+++ jquery.js
@@ -1,6 +1,14 @@
+$( document ).ready(function() {
+  
 
-$('.hamburger-button').click(function(){
+  $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
     $(this).toggleClass('active');
     
   });
+
+  AOS.init({
+    duration: 500 // values from 0 to 3000, with step 50ms
+  });
+
+});
